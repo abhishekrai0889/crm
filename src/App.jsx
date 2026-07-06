5f7a0f0258a83e7a1924cc3e2b991720a056a938
abhishekrai0889 <abhishek.rai@engeniatech.in>
2026-07-06 19:14:24 +0530

    table

--- src/App.jsx
+++ src/App.jsx
@@ -189,7 +189,6 @@ function App() {
           <Route path="notifications" element={<Notifications />} />
           <Route path="notifications/:id" element={<Notifications />} />
           <Route path="team" element={<Team />} />
-          <Route path="settings/billing" element={<Billing />} />
           <Route path="organizations" element={<BasicTable />} />
           {/* Unbuilt sections land on an in-layout placeholder */}
           <Route path="*" element={<ComingSoon />} />
